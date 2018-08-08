d9d520716f9f214a14b93122629f904ed21af988
Muhammed El-Hadidy <mohammed.elhadidy90@gmail.com>
2018-08-08 18:56:35 +0200

    feat: added header element instead of div.

--- src/Header.js
+++ src/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = props => {
     return (
-        <div className="header">
+        <header className="header">
             <h1>Neighborhood Map</h1>
-        </div>
+        </header>
     )
 }
 
